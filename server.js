ca1ab32a56da83b466a77079327de84eda1cdd17
shadowmoonezios <agushifelde@hotmail.com>
2025-10-06 21:19:20 +0300

    Added logging for server startup

--- server.js
+++ server.js
@@ -21,4 +21,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`API ready and listening...`);
 });
