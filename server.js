3cc72b7a09f28216b54174fd5214e6bfff5eca72
shadowmoonezios <agushifelde@hotmail.com>
2025-10-06 21:16:49 +0300

    Fix MongoDB connection URI for environment variable support.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/lieblingsgerichte', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/lieblingsgerichte', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
