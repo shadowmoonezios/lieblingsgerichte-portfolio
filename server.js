7d12f3c0bb8126870e894408245003a719b505f0
shadowmoonezios <agushifelde@hotmail.com>
2025-10-06 21:16:11 +0300

    Fix comment translation to English in server.js

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Verbindung
+// MongoDB connection
 mongoose.connect('mongodb://localhost:27017/lieblingsgerichte', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => console.log('MongoDB verbunden')).catch(err => console.log(err));
+}).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server läuft auf Port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
